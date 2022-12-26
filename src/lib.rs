30d0548d6f22dc13cfe5034485397d6c716798cf
Alfred-Park <alfred.park.st@gmail.com>
2022-12-26 13:29:48 +0900

    Confirm on localhost

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,63 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+extern crate num;
+
+fn dot_sub<T>(a: &Vec<Vec<T>>, b: &Vec<Vec<T>>) -> Option<Vec<Vec<T>>>
+where
+    T: num::Num + Clone + Copy,
+{
+    if a.len() == 0 || a[0].len() != b.len() {
+        return None;
+    }
+
+    let mut res = vec![vec![T::zero(); a.len()]; b[0].len()];
+    for i in 0..a.len() {
+        for k in 0..a[0].len() {
+            for j in 0..b[0].len() {
+                res[i][j] = res[i][j] + a[i][k] * b[k][j];
+            }
+        }
+    }
+    Some(res)
+}
+
+const MAX: usize = 1024;
+
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Matrix {
+    result: [f64; MAX],
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-matrix!");
+impl Matrix {
+    pub fn new() -> Matrix {
+        Matrix { result: [0.0; MAX] }
+    }
+
+    pub fn get_result_ptr(&self) -> *const f64 {
+        self.result.as_ptr()
+    }
+
+    pub fn dot(&mut self, a: JsValue, b: JsValue, di: u8, dk: u8, dj: u8) {
+        utils::set_panic_hook();
+        let (di, dk, dj) = (di as usize, dk as usize, dj as usize);
+        let a: Vec<f64> = a.into_serde().unwrap();
+        let b: Vec<f64> = b.into_serde().unwrap();
+
+        let a = (0..di)
+            .map(|i| (0..dk).map(|k| a[i * dk + k]).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        let b = (0..dk)
+            .map(|k| (0..dj).map(|j| b[k * dj + j]).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        dot_sub(&a, &b)
+            .unwrap()
+            .iter()
+            .flatten()
+            .enumerate()
+            .for_each(|(i, &v)| self.result[i] = v);
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,91 @@
-import * as wasm from "wasm-matrix";
+import { Matrix } from "wasm-matrix";
+import { memory } from "wasm-matrix/wasm_matrix_bg.wasm";
 
-wasm.greet();
+const set_matrix = () => {
+    const tb_a = document.querySelector("#mat-a tbody");
+    const tb_b = document.querySelector("#mat-b tbody");
+    const tb_c = document.querySelector("#mat-c tbody");
+
+    const di = parseInt(document.querySelector("#di").value);
+    const dk = parseInt(document.querySelector("#dk").value);
+    const dj = parseInt(document.querySelector("#dj").value);
+
+    tb_a.innerHTML = "";
+    tb_b.innerHTML = "";
+    tb_c.innerHTML = "";
+
+    for (let i = 0; i < di; i++) {
+        const tr = document.createElement("tr");
+        for (let k = 0; k < dk; k++) {
+            const td = document.createElement("td");
+            td.innerHTML = `<input class="mat-inp" type="number" value="0" />`;
+            tr.appendChild(td);
+        }
+        tb_a.appendChild(tr);
+    }
+
+    for (let k = 0; k < dk; k++) {
+        const tr = document.createElement("tr");
+        for (let j = 0; j < dj; j++) {
+            const td = document.createElement("td");
+            td.innerHTML = `<input class="mat-inp" type="number" value="0" />`;
+            tr.appendChild(td);
+        }
+        tb_b.appendChild(tr);
+    }
+
+    for (let i = 0; i < di; i++) {
+        const tr = document.createElement("tr");
+        for (let j = 0; j < dj; j++) {
+            const td = document.createElement("td");
+            td.setAttribute("id", `c${i}-${j}`);
+            td.innerHTML = "0";
+            tr.appendChild(td);
+        }
+        tb_c.appendChild(tr);
+    }
+};
+
+const calc_matrix = (mat) => {
+    const tb_a = document.querySelector("#mat-a tbody");
+    const tb_b = document.querySelector("#mat-b tbody");
+    const tb_c = document.querySelector("#mat-c tbody");
+
+    const di = parseInt(document.querySelector("#di").value);
+    const dk = parseInt(document.querySelector("#dk").value);
+    const dj = parseInt(document.querySelector("#dj").value);
+
+    if (di < 0 || dk < 0 || dj < 0 || di > 32 || dk > 32 || dj > 32) return;
+
+    const a = Array.from(tb_a.querySelectorAll("td input")).map(e => {
+        const res = parseFloat(e.value);
+        return res ? res : 0.0;
+    });
+
+    const b = Array.from(tb_b.querySelectorAll("td input")).map(e => {
+        const res = parseFloat(e.value);
+        return res ? res : 0.0;
+    });
+
+    mat.dot(a, b, di, dk, dj);
+
+    const c = new Float64Array(memory.buffer, mat.get_result_ptr(), di * dj);
+    tb_c.querySelectorAll("td").forEach((e, i) => e.innerHTML = c[i]);
+};
+
+const main = () => {
+    const matrix = Matrix.new();
+
+    set_matrix();
+    calc_matrix(matrix);
+
+    addEventListener("change", e => {
+        if (e.target.classList.contains("config")) {
+            set_matrix();
+        } else {
+            calc_matrix(matrix);
+        }
+    }, false);
+};
+
+main();
